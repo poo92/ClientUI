450dd2fae10d3f8bc74ff0b2f8533141bdda5e0d
Poornima Karunarathne <PoornimaK@99x.lk>
2018-03-26 16:55:28 +0530

    homepage edited

--- ClientApp/app/components/addclient/addclient.ts
+++ ClientApp/app/components/addclient/addclient.ts
@@ -2,14 +2,19 @@
 import { inject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { ClientHelper } from '../helpers/clienthelper';
-import { ValidationControllerFactory, ValidationRules, ValidationController } from 'aurelia-validation';
+import { Client } from '../helpers/client';
+import { ValidationControllerFactory, ValidationRules, ValidationController, Validator, validateTrigger } from 'aurelia-validation';
 
-@inject(HttpClient, ClientHelper, Router, ValidationControllerFactory)
+@inject(HttpClient, ClientHelper, Router, ValidationControllerFactory, Validator)
 export class AddClient {
 	public httpClient: HttpClient;
 	public clientHelper: ClientHelper;
 	public router: Router;
 	public controller: ValidationController;
+	public rules: any;
+	public canSave: boolean;
+	public validator: Validator;
+	public client: Client = new Client();
 
 	public clientName: string = "";
 	public clientId: string = "";
@@ -24,44 +29,59 @@ export class AddClient {
 	public selectedApiResources: Array<number> = [];
 	public allowedScopes: Array<number> = [];
 
-	constructor(httpClient: HttpClient, clientHelper: ClientHelper, router: Router, controllerFactory: ValidationControllerFactory) {
+	constructor(httpClient: HttpClient, clientHelper: ClientHelper, router: Router, controllerFactory: ValidationControllerFactory, validator: Validator) {
 		this.httpClient = httpClient;
-		this.clientHelper = clientHelper;	
+		this.clientHelper = clientHelper;
 		this.router = router;
-		this.controller = controllerFactory.createForCurrentScope();
+		this.validator = validator;
+		this.controller = controllerFactory.createForCurrentScope(validator);
 
+		//this.controller = controllerFactory.createForCurrentScope();
 		ValidationRules
 			.ensure('clientId').required()
 			.ensure('clientName').required()
-			.on(this);
-	}	
+			.ensure('clientSecret').required()
+			.ensure('clientUri').required()
+			.on(this.client);
 
-	public add() {
-		for (let resource of this.selectedIdentityResources) {
-			this.allowedScopes.push(resource);
-		}
-		for (let resource of this.selectedApiResources) {
-			this.allowedScopes.push(resource);
-		}
-
-		var client = { ClientId: this.clientId, ClientName: this.clientName, ClientSecret: this.clientSecret, GrantType: this.grantType, ClientProperty: this.clientProperty, AllowedScopes: this.allowedScopes, ClientUri: this.clientUri, RedirectUrl: this.redirectUrl, FrontChannelLogoutUrl: this.frontChannelLogoutUrl, PostLogoutUrl: this.postLogoutUrl };
-				
-		this.httpClient.fetch('api/client/addclient',
-			{
-				method: "POST",
-				body: JSON.stringify(client),
-				headers: {					
-					'Content-Type': 'application/json'
-				}
-			})
-			.then(result => result.json())
-			.then(data => {
-				if (data == "ok") {
-					alert("Client Successfully Added.");
-					this.router.navigateToRoute('viewallclients')
-				}
-			});
-
+		this.canSave = false;
+		this.controller.validateTrigger = validateTrigger.changeOrBlur;
+		this.controller.subscribe(event => this.validateWhole());
 	}
-	
+
+
+	private validateWhole() {
+		this.validator.validateObject(this.client)
+			.then(results => this.canSave = results.every(result => result.valid));
+		console.log(this.controller.errors);
+	}
+
+	public add() {		
+			for (let resource of this.selectedIdentityResources) {
+				this.allowedScopes.push(resource);
+			}
+			for (let resource of this.selectedApiResources) {
+				this.allowedScopes.push(resource);
+			}
+
+		var client = { ClientId: this.client.clientId, ClientName: this.client.clientName, ClientSecret: this.client.clientSecret, GrantType: this.client.grantType, ClientProperty: this.client.clientProperty, AllowedScopes: this.allowedScopes, ClientUri: this.client.clientUri, RedirectUrl: this.client.redirectUrl, FrontChannelLogoutUrl: this.client.frontChannelLogoutUrl, PostLogoutUrl: this.client.postLogoutUrl };
+
+			this.httpClient.fetch('api/client/addclient',
+				{
+					method: "POST",
+					body: JSON.stringify(client),
+					headers: {					
+						'Content-Type': 'application/json'
+					}
+				})
+				.then(result => result.json())
+				.then(data => {
+					if (data == "ok") {
+						alert("Client Successfully Added.");
+						this.router.navigateToRoute('viewallclients')
+					}
+				});
+
+		
+	}
 }
